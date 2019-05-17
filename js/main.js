89a995976c6dc8aae3a60e83f5ee864cf6250561
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-05-17 17:45:27 +0100

    Add main image preview and format layouts

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const body = document.querySelector('body')
 const menuButton = document.querySelector('#overlay-button')
 const aboutButton = document.querySelector('.triangle-bottom')
 
+const lightGallery = window.lightGallery
+
 function toggleOverlay () {
   body.classList.toggle('menu-active')
   document.querySelector('html').classList.toggle('fixed')
@@ -18,8 +20,28 @@ if (aboutButton) {
 }
 
 // Portfolio grid
-if (document.querySelector('.subportfolio-grid')) {
+if (lightGallery && document.querySelector('.subportfolio-grid')) {
   lightGallery(document.querySelector('.subportfolio-grid'), {
+    subHtmlSelectorRelative: true,
+    speed: 600,
+    counter: false,
+    showAfterLoad: true,
+    download: false,
+    cssEasing: 'ease-in-out',
+    swipeThreshold: 25,
+    mode: 'lg-slide',
+    hideBarsDelay: 99999999,
+    youtubePlayerParams: {
+      autoplay: 0
+    },
+    preload: 0,
+    autoplay: false
+  })
+}
+
+// Portfolio home page images
+if (lightGallery && document.querySelector('.main-images, .main-image')) {
+  lightGallery(document.querySelector('.main-images, .main-image'), {
     subHtmlSelectorRelative: true,
     speed: 600,
     counter: false,
